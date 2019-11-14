485668aa3b0f753ad5490d2b856d2c2a70f9e4eb
MisterMur <brian.j.murillo@gmail.com>
2019-11-14 12:45:40 -0500

    fix css errors

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,5 +1,5 @@
 import React,{Fragment} from "react"
-import {  Route } from 'react-router-dom'
+import  Route from 'react-router-dom'
 import Profile from './Profile.js'
 import Leader from './Leader.js'
 
