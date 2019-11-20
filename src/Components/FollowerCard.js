4e853a3192fd9e478cd37b71c15376cf4e2ed5b3
MisterMur <brian.j.murillo@gmail.com>
2019-11-20 18:58:08 -0500

    fix follower card size issue

--- src/Components/FollowerCard.js
+++ src/Components/FollowerCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link,Route,Router} from 'react-router-dom'
 import Profile from './Profile.js'
-
+import {Button,Card} from 'react-bootstrap'
 
 
 var randImg = 'http://lorempixel.com/400/200/sports/'
@@ -10,52 +10,67 @@ class FollowerCard extends React.Component {
   constructor(props){
     super(props)
   }
-  // handleUserClick=(e)=>{
-  //   // debugger
-  //   console.log('in handle user click ',this.props.follower)
-  //   return (
-  //     // <Route path={`/profile/${user.id}`} exact render= {() => {
-  //         <Profile
-  //           user={this.props.follower}
-  //         />
-  //   )
-  // }
-  // <Link to={`/profile/${this.props.follower.id}`}>
-  // </Link>
+
+  // return (
+  //   <div class="card follower-container" >
+  //     <img class="card-img-top" src={randImg} alt="Card image cap"/>
+  //     <div class="card-body">
+  //       <h5 class="card-title">Name: {this.props.follower.name}</h5>
+  //       <p class="card-text">I drive a: {this.props.follower.car}</p>
+  //
+  //       <div onClick={(e,user)=>this.props.handleUserClick(e,this.props.follower)}>
+  //         <Link to={`/profile/${this.props.follower.id}`}>
+  //           <button
+  //             type="button"
+  //             class="btn btn-primary"
+  //             >
+  //             Check out my profile!</button>
+  //         </Link>
+  //
+  //       </div>
+  //
+  //     </div>
+  //   </div>
+  //
+  //
+  // )
 
 
-
-
-  // <Route path={`/profile/${this.props.follower.id}`} user={this.props.follower} />
-  // <button
-  //   type="button"
-  //   class="btn btn-primary"
-  //   >
-  //   Check out my profile!</button>
-  render() {
+  renderCard=()=>{
     return (
-      <div class="card follower-container" >
-        <img class="card-img-top" src={randImg} alt="Card image cap"/>
-        <div class="card-body">
-          <h5 class="card-title">Name: {this.props.follower.name}</h5>
-          <p class="card-text">I drive a: {this.props.follower.car}</p>
-
+      <>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={randImg} />
+        <Card.Body>
+          <Card.Title>Name: {this.props.follower.name}</Card.Title>
+          <Card.Text>
+            I drive a: {this.props.follower.car}
+          </Card.Text>
           <div onClick={(e,user)=>this.props.handleUserClick(e,this.props.follower)}>
             <Link to={`/profile/${this.props.follower.id}`}>
-              <button
+              <Button
                 type="button"
                 class="btn btn-primary"
                 >
-                Check out my profile!</button>
+                Check out my profile!</Button>
             </Link>
-
           </div>
+        </Card.Body>
+      </Card>
+      </>
+    )
+  }
 
-        </div>
+
+
+  render() {
+    return (
+      <div class="card follower-container" >
+        {this.renderCard()}
       </div>
 
-
     )
+
   }
 }
 export default FollowerCard
