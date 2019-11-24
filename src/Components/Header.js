979ca2e3fd0eb8cba5a474d50c4610299b24ef36
MisterMur <brian.j.murillo@gmail.com>
2019-11-23 19:15:36 -0500

    create new app and redo header file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,68 @@ import React from "react";
 
 class Header extends React.Component {
 
-  render() {
+
+  renderProfileLink = () => {
+    // console.log('in render profile link')
+    // debugger
+    if(this.props.currentUser){
+      console.log(this.props)
+      return <Link to={`/profile/${this.props.currentUser.id}`}>Profile</Link>
+    }
+  }
+
+  renderNav=()=>{
+    return(
+      <>
+          <Navbar bg="dark" expand="md">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+
+              <Nav pullRight>
+                <NavItem componentClass='span'>
+                  {this.renderProfileLink()}
+                </NavItem>
+                <NavItem componentClass='span'>
+                  <Link to="/rides">Rides</Link>
+                </NavItem>
+                <NavItem componentClass='span'>
+                  <Link to="/forums">Forums</Link>
+                </NavItem>
+              </Nav>
+
+            </Navbar.Collapse>
+          </Navbar>
+      </>
+    )
+  }
+  renderHeader=()=>{
     return (
+      <>
       <header className="header">
         <div className="container col-11" >
           <b>RideShare</b>
         </div>
       </header>
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <>
+      {this.renderHeader()}
+      {this.renderNav()}
+      </>
     )
   }
 }
-export default Header
+
+function mapStateToProps(state) {
+  // maps the state from the store to the props
+  return {
+    currentUser: null
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
