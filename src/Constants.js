5c269ea4300f70feaa27b65675f74d42871802d9
MisterMur <brian.j.murillo@gmail.com>
2019-12-02 05:43:36 -0500

    fix prod and dev url

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,6 @@
 //development vals
+export const prodURL = 'http://ride-share-api.herokuapp.com/api/v1/'
+// export const prodURL = 'http://localhost:3000/api/v1/'
 export  const USERURL='http://localhost:3000/api/v1/users'
 export  const COMPANYURL ='http://localhost:3000/api/v1/companies'
 export  const RIDEURL = 'http://localhost:3000/api/v1/rides'
@@ -9,7 +11,15 @@ export const LOGINURL = 'http://localhost:3000/api/v1/login'
 export const AUTOLOGINURL = 'http://localhost:3000/api/v1/auto_login'
 
 //production vals
-// export  const USERURL='http://ride-share-api.herokuapp.com/api/v1/users/'
+
+export const USERURL=prodURL+'users/'
+export const COMPANYURL=prodURL+'companies/'
+export const RIDESURL=prodURL+'rides/'
+export const FRIENDSHIPURL=prodURL+'friendships/'
+export const FORUMSURL = prodURL+'forums/'
+export const LOGINURL = prodURL+'login/'
+export const AUTOLOGINURL = prodURL+'auto_login/'
+
 // export  const COMPANYURL = 'http://ride-share-api.herokuapp.com/api/v1/companies/'
 // export  const RIDEURL = 'http://ride-share-api.herokuapp.com/api/v1/rides/'
 // export const FRIENDSHIPURL = 'http://ride-share-api.herokuapp.com/api/v1/friendships'
