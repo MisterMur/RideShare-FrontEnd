a47cd6e8b35d3d4cd762f7d5d31f3ea58e50fe83
MisterMur <brian.j.murillo@gmail.com>
2019-12-02 05:04:18 -0500

    after merge

---
+++ src/Constants.js
@@ -0,0 +1,26 @@
+//development vals
+export  const USERURL='http://localhost:3000/api/v1/users'
+export  const COMPANYURL ='http://localhost:3000/api/v1/companies'
+export  const RIDEURL = 'http://localhost:3000/api/v1/rides'
+export  const FORUMSURL = 'http://localhost:3000/api/v1/forums'
+
+export const FRIENDSHIPURL = 'http://localhost:3000/api/v1/friendships'
+export const LOGINURL = 'http://localhost:3000/api/v1/login'
+export const AUTOLOGINURL = 'http://localhost:3000/api/v1/auto_login'
+
+//production vals
+// export  const USERURL='http://ride-share-api.herokuapp.com/api/v1/users/'
+// export  const COMPANYURL = 'http://ride-share-api.herokuapp.com/api/v1/companies/'
+// export  const RIDEURL = 'http://ride-share-api.herokuapp.com/api/v1/rides/'
+// export const FRIENDSHIPURL = 'http://ride-share-api.herokuapp.com/api/v1/friendships'
+
+export const ADD_USER='ADD_USER'
+export const LOGIN_USER='LOGIN_USER'
+export const LOGOUT_USER='LOGOUT_USER'
+export const REMOVE_FOLLOWER='REMOVE_FOLLOWER'
+export const ADD_FOLLOWER='ADD_FOLLOWER'
+export const FETCH_ALL_RIDES='FETCH_ALL_RIDES'
+export const FETCH_ALL_FORUMS='FETCH_ALL_FORUMS'
+export const FETCH_ALL_USERS='FETCH_ALL_USERS'
+export const FETCH_ALL_COMPANIES='FETCH_ALL_COMAPNIES'
+export const FETCH_CURRENT_USER='FETCH_CURRENT_USER'
