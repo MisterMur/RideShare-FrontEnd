e0039599114f2a870626c727423b5e41094fafcb
MisterMur <brian.j.murillo@gmail.com>
2020-01-26 20:06:52 -0500

    file upload start:

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -3,12 +3,16 @@ import { Form, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import {createUser} from '../Actions';
 
+import ImageUploader from 'react-images-upload';
+
+
 class SignupForm extends React.Component {
 	state = {
 		name: "",
 		username: "",
 		password: "",
 		passwordConfirmation: "",
+		// profilePic:null,
 	}
 
 	UNSAFE_componentWillReceiveProps(newProps) {
@@ -23,28 +27,6 @@ class SignupForm extends React.Component {
 		})
 	}
 
-	// createUser = (login_data) => {
-	// 	fetch(USERURL, {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json",
-	// 			"Accepts": "application/json",
-	// 		},
-	// 		body: JSON.stringify(login_data)
-	// 	})
-	// 	.then(res => res.json())
-	// 	.then((response) => {
-	//
-	// 		if (response.error){
-	// 			alert(response.error)
-	// 		} else {
-	// 			// debugger
-	// 			this.props.setCurrentUser(response.user)
-	// 			localStorage.setItem('jwt', response.jwt)
-	// 			this.props.history.push(`/users/${response.user.id}`)
-	// 		}
-	// 	})
-	// }
 
 	handleSubmit = () => {
 		if(this.state.password === this.state.passwordConfirmation){
@@ -55,9 +37,30 @@ class SignupForm extends React.Component {
 			alert("Passwords don't match!")
 		}
 	}
-// 	handleSubmit = event => {
-// 	event.preventDefault()
-// }
+
+	fileSelectedHandler = (event)=>{
+		this.setState({selectedFile:event.target.files[0]})
+	}
+	fileUploadHandler=()=>{
+
+	}
+
+	// onDrop=(pic) =>{
+	// 	console.log('Setting state profilePic to ',pic)
+  //       this.setState({
+  //           profilePic: pic[0],
+  //       });
+  //   }
+	// <ImageUploader
+	// 			withIcon={true}
+	// 			withLabel={true}
+	// 			label='Profile Picture'
+	// 			buttonText='Choose image'
+	// 			onChange={this.onDrop}
+	// 			imgExtension={['.jpg', '.gif', '.png', '.gif']}
+	// 			maxFileSize={5242880}
+	// 			singleImage={true}
+	// 	/>
 
 	render(){
 		return (<>
@@ -78,11 +81,17 @@ class SignupForm extends React.Component {
 		      <label>Password Confirmation</label>
 		      <input onChange={this.handleChange} type="password" name="passwordConfirmation" value={this.state.passwordConfirmation} placeholder='Password Confirmation' />
 		    </Form.Field>
+				<Form.Field>
+
+
+				</Form.Field>
 		    <Button type='submit'>Submit</Button>
 		  </Form></>
 		)
 	}
 }
+// <input type="file" onChange={this.fileSelectedHandler}/>
+// <button onClick={this.fileUploadHandler}></button>
 function mapStateToProps(state) {
   // maps the state from the store to the props
   const {user} = state
