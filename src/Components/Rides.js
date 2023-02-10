bdf104dd0e925483886d23e339ed8947cc125039
Brian Murillo <brian.j.murillo@gmail.com>
2023-02-09 18:06:42 -0600

    style: restyle rides search filter sort

--- src/Components/Rides.js
+++ src/Components/Rides.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { connect } from 'react-redux';
+
+import styled from 'styled-components'
+import Dropdown from 'react-bootstrap/Dropdown';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+
+import {fetchUsers,fetchRides} from '../Actions';
+
 import Leaderboard from './Leaderboard.js'
 import RideList from './RideList'
-import { connect } from 'react-redux';
-import {fetchUsers,fetchRides} from '../Actions';
+
+import {devicesMax } from '.././Globals/Breakpoints'
 
 
 
@@ -14,7 +28,9 @@ class Rides extends React.Component {
       filteredRides: this.props.rides,
       users:[],
       textInput: '',
-      friendFilter:false
+      friendFilter:false,
+      sortBy:'Recent',
+      filterByCompany:'All Companies'
     }
 
   componentDidMount(){
@@ -31,58 +47,112 @@ class Rides extends React.Component {
   }
 
   returnDropdown = () => {
+    const {sortBy,filterByCompany} = this.state;
     return (
       <div className="">
-      <form>
-      <select name="cars"
-      onChange={this.handleSortSubmit}
-      >
-        <option value="recent">Most Recent</option>
-        <option value="price">Price</option>
-        <option value="distance">Distance</option>
-      </select>
-      <select name="cars"
-      onChange={this.handleFilterSubmit}
-      >
-        <option value="all">All</option>
-        <option value="uber">Uber</option>
-        <option value="lyft">Lyft</option>
+        <form>
+          <SearchRideInput
+            type="text"
+            onChange={this.handleSearch}
+            value={this.state.textInput}
+            placeholder="Search by location"
+          />
+          {/* <div class='max-w-md mx-auto'>
+            <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+              <div class="grid place-items-center h-full w-12 text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </div>
+              <input
+                class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                type="text"
+                id="search"
+                placeholder="Search something.." /> 
+            </div>
+          </div> */}
+          
+      
+          <SearchOptions>
+            <FilterDropdownGroup radioBtn={this.props.currentUser}>
+              <Dropdown>
+                <Dropdown.Toggle style={{width: this.props.currentUser? '200px':'100%'}} variant="primary" id="dropdown-sort-rides">
+                  Sort By {sortBy}
+                </Dropdown.Toggle>
+                <Dropdown.Menu name="sort-rides"  >
+                  <Dropdown.Item onClick={ ()=>this.handleSortSubmit('Recent')} value="Recent">Most Recent</Dropdown.Item>
+                  <Dropdown.Item onClick={()=>this.handleSortSubmit('Price')} value="Price">Price</Dropdown.Item>
+                  <Dropdown.Item onClick={()=>this.handleSortSubmit('Distance')} value="Distance">Distance</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown>
+                <Dropdown.Toggle style={{width: this.props.currentUser? '200px':'100%'}}variant="primary" id="dropdown-filter-company">
+                  Filter By {filterByCompany}
+                </Dropdown.Toggle>
+                <Dropdown.Menu name="filter-company"  >
+                  <Dropdown.Item onClick={ ()=>this.handleFilterSubmit('All Companies')} value="All">All Companies</Dropdown.Item>
+                  <Dropdown.Item onClick={()=>this.handleFilterSubmit('Uber')} value="Uber">Uber</Dropdown.Item>
+                  <Dropdown.Item onClick={()=>this.handleFilterSubmit('Lyft')} value="Lyft">Lyft</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </FilterDropdownGroup>
+            {this.props.currentUser? (
+            <RadioBtnSearchGroup>
+              <FormGroup row>
+                <FormControlLabel
+                  control={<Checkbox
+                      icon={<FavoriteBorder />}
+                      checkedIcon={<Favorite />}
+                      checked={this.state.friendFilter}
+                      onChange={this.handleFriendFilter}
+                      name="checkedA" />}
+                  id="rides-radio-btn"
+                  label="Friends"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={!this.state.friendFilter}
+                      onChange={this.handleChangeBack}
+                      name="NoFilter"
+                      color="primary"
+                    />
+                  }
+                  id="rides-radio-btn"
+                  label="All"
+                />
 
-      </select>
-        <input
-         type="text"
-         onChange={this.handleSearch}
-         value={this.state.textInput}
-         placeholder="Search by location"
-         />
-         {this.props.currentUser? (
-          <form>
-              <input type="radio" name="filter"  value="friends" onChange={this.handleFriendFilter}/> Friends
-              <input type="radio" name="filter"  value="all" onChange={this.handleChangeBack}/> All
-          </form>
+              </FormGroup>
+                
+            </RadioBtnSearchGroup>
+            ): null}
 
-         ): null}
+
+          </SearchOptions>
+       
+
       </form>
       </div>
     )
   }
 
   handleChangeBack = () => {
-    this.setState({filteredRides: this.state.allRides})
+    this.setState({filteredRides: this.state.allRides,friendFilter:false})
   };
 
   handleFriendFilter = () => {
-    if(this.props.currentUser){
-      let myFriends = this.props.currentUser.followers.map(follower => follower.id)
-      let filtered =[]
-      this.state.allRides.map((ride) =>
-      {
-        return myFriends.map((friend)=>
-           ride.user_id === friend ? filtered.push(ride) : null
-        )
-      }
-    )
-    this.setState({filteredRides: filtered})
+    const {currentUser} = this.props;
+    const {friendFilter, allRides} = this.state;
+
+    if(currentUser){
+
+        let myFriends = currentUser.followers.map(follower => follower.id)
+        let filtered =[]
+        allRides.map((ride) =>{
+          return myFriends.map((friend)=>ride.user_id === friend ? filtered.push(ride) : null)
+        })
+        this.setState({filteredRides:filtered, friendFilter:true})
+
     }
   };
 
@@ -94,41 +164,41 @@ class Rides extends React.Component {
     this.setState({filteredRides: filtered})
   }
 
-  handleSortSubmit = (e) => {
-    e.preventDefault()
-    if (e.target.value === "distance"){
-      let filtered = this.state.filteredRides.sort((a, b) => (a.distance < b.distance)? 1 : -1)
-      this.setState({filteredRides: filtered})
+  handleSortSubmit = (sortBy) => {
+    console.log('in handle sort')
+    if(sortBy !== this.state.sortBy){
+      this.setState({sortBy})
+      if (sortBy === "Distance"){
+        let filtered = this.state.filteredRides.sort((a, b) => (a.distance < b.distance)? 1 : -1)
+        this.setState({filteredRides: filtered})
+      }
+      else if (sortBy === "Price"){
+        let filtered = this.state.filteredRides.sort((a, b) => (a.price < b.price)? 1 : -1)
+        this.setState({filteredRides: filtered})
+      }
+      else if (sortBy === "Recent"){
+        let filtered = this.state.filteredRides.sort((a, b) => (a.end_at < b.end_at)? 1 : -1)
+        this.setState({filteredRides: filtered})
+      }
     }
-    else if (e.target.value === "price"){
-      let filtered = this.state.filteredRides.sort((a, b) => (a.price < b.price)? 1 : -1)
-      this.setState({filteredRides: filtered})
-    }
-    //DID NOT TEST THIS YET
-    else if (e.target.value === "recent"){
-      let filtered = this.state.filteredRides.sort((a, b) => (a.end_at > b.end_at)? 1 : -1)
-      this.setState({filteredRides: filtered})
-    }
-    ////////
-  }
+  };
 
-  handleFilterSubmit = (e) => {
-    e.preventDefault()
-
-    if (e.target.value === "all"){
-      this.setState({filteredRides: this.state.allRides})
+  handleFilterSubmit = (filterByCompany) => {
+    if(filterByCompany !== this.state.filterByCompany){
+      this.setState({filterByCompany})
+      if (filterByCompany === "All Companies"){
+        this.setState({filteredRides: this.state.allRides})
+      }
+      else if (filterByCompany === "Uber"){
+        let filtered = this.state.allRides.filter(ride => ride.company.name === filterByCompany)
+        this.setState({filteredRides: filtered})
+      }
+      else if (filterByCompany === "Lyft"){
+        let filtered = this.state.allRides.filter(ride => ride.company.name === filterByCompany)
+        this.setState({filteredRides: filtered})
+      }
     }
-
-    else if (e.target.value === "uber"){
-      let filtered = this.state.allRides.filter(ride => ride.company_id === 1)
-      this.setState({filteredRides: filtered})
-    }
-
-    else if (e.target.value === "lyft"){
-      let filtered = this.state.allRides.filter(ride => ride.company_id === 2)
-      this.setState({filteredRides: filtered})
-    }
-  }
+  };
 
   render() {
 
@@ -174,3 +244,57 @@ function mapStateToProps(state) {
   }
 }
 export default connect(mapStateToProps,mapDispatchToProps) (Rides)
+
+const FilterDropdownGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: ${props => props.radioBtn ? "space-evenly" : "space-between"};
+
+  @media only screen and ${devicesMax.mobileL} {
+    flex-direction: ${props => props.radioBtn ? "column" : "row"};
+
+  }
+`;
+
+const RadioBtnSearchGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  width:100%;
+  justify-content: flex-end;
+
+  @media only screen and ${devicesMax.mobileL} {
+    flex-direction: row;
+    justify-content: flex-end;
+
+  }
+`;
+
+const SearchOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 15px 0;
+
+  @media only screen and ${devicesMax.mobileL} {
+    flex-direction: row;
+  }
+`;
+
+const SearchRideInput = styled.input`
+  width: 100%;
+  padding: 0.5em 0;
+  margin-top: 0.5em;
+  align-items: center;
+  color: black;
+  font-weight:200px;
+  background-color: #ecf0f1;
+  border-radius: 100px;
+  border-color: #007bff;
+  box-sizing: border-box;
+  display: flex;
+  max-height: none;
+  min-height: 48px;
+  padding-left: 17px;
+  padding-right: 17px;
+  position: relative;
+`;
